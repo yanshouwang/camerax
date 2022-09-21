8a974faea93e1488dd7cb39ff9dfaa6239b3c493
iAMD <yanshouwang@outlook.com>
2022-09-21 13:25:52 +0800

    fix: 适配 flutter 3.0 (#13)

--- android/src/main/kotlin/dev/yanshouwang/camerax/Util.kt
+++ android/src/main/kotlin/dev/yanshouwang/camerax/Util.kt
@@ -3,7 +3,7 @@ package dev.yanshouwang.camerax
 import android.graphics.Point
 import androidx.camera.core.Camera
 import androidx.camera.core.ImageProxy
-import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.common.Barcode
 
 val Any.TAG: String
     get() = javaClass.simpleName
