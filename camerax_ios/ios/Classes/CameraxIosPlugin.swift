226abb0ca04ec9b3295c1f56b0b7c3895ef15e00
yanshouwang <yanshouwang@hebei.dev>
2025-01-15 10:57:36 +0800

    Remove Classes

--- camerax_ios/ios/Classes/CameraxIosPlugin.swift
+++
@@ -1,8 +0,0 @@
-import Flutter
-import UIKit
-
-public class CameraXiOSPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-      registrar.register(PreviewViewFactory.shared, withId: "hebei.dev/PreviewView")
-  }
-}
